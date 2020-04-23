f6db52a8651e3ec7f02ea0f2bd43c9622a54b49b
Junsung Park <zeipar@gmail.com>
2020-04-23 03:07:09 -0700

    added testing for dayListItem for mulitiple clicks with async behaviors

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, getByAltText, prettyDOM, getAllByTestId, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, getByAltText, prettyDOM, getAllByTestId, getByPlaceholderText, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -17,12 +17,11 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
     
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
     const appointments = getAllByTestId(container, "appointment");
-    
     const appointment = appointments[0];
     
     fireEvent.click(getByAltText(appointment, "Add"));
@@ -32,10 +31,15 @@ describe("Application", () => {
     });
 
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-
     fireEvent.click(getByText(appointment, "Save"));
+  
+    expect(getByText(appointment, /saving/i)).toBeInTheDocument();
 
-    console.log(prettyDOM(appointment));
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
 
+    const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+    
   });
 })
