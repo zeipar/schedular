f316c72bb05d69765dcd8976163086cfdb11e1ce
Junsung Park <zeipar@gmail.com>
2020-04-22 20:34:52 -0700

    used immutable state instead of changing the state itself

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
   const [mode, setMode] = useState(initial);
 
-  function transition (newMode, replace = false){
-    if (!replace){
-      history.push(newMode);
-    }
+  const transition = (newMode, replace = false) => {
     setMode(newMode);
+    if (replace){
+      setHistory(prev => [...[...prev.slice(0, prev.length - 1)], newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
   };
 
   function back (){
     if (history.length > 1) {
-      history.pop();
-      setMode(history[history.length - 1]);
+      setMode(history[history.length - 2]);
+      setHistory(prev => [...prev.slice(0, prev.length - 1)]);
     }
   };
 
