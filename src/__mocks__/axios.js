e324daa3d329cb99846c6553a0a453aa2cc18e04
Junsung Park <zeipar@gmail.com>
2020-04-23 01:51:31 -0700

    created mock API server

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -52,3 +52,34 @@ const fixtures = {
     }
   }
 };
+
+export default {
+  get: jest.fn(url => {
+    if (url === "/api/days") {
+      /* Resolve days data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.days
+      });
+    }
+
+    if (url === "/api/appointments") {
+      /* Resolve appointments data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments
+      });
+    }
+
+    if (url === "/api/interviewers") {
+      /* Resolve interviewers data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers
+      });
+    }
+  })
+}
