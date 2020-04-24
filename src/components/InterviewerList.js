d48a19855971807713ab7ecb7f61f61c6a0705f5
Junsung Park <zeipar@gmail.com>
2020-04-23 18:17:17 -0700

    added data-testid

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -23,7 +23,7 @@ export default function InterviewerList(props) {
   });
 
   return (
-    <section className="interviewers">
+    <section className="interviewers" data-testid="interviewer">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
         { interviewers }
