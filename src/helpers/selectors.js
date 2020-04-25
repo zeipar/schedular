d057e576c39beb9a0fcc8077acbb3a98b5858bf3
Junsung Park <zeipar@gmail.com>
2020-04-24 23:33:24 -0700

    refactored and linted

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,32 +1,32 @@
-export function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, selectedDay) {
+
+  // if received no days object, return an empty array
   if (state.days.length === 0) {
     return [];
   }
 
-  const filteredDays = state.days.filter(item => item.name === day);
-  if (filteredDays.length === 0) {
-    return [];
-  };
+  // return an empty array if there is no match, otherwise return an array of matching objects
+  const matchingAppts = state.days.filter(day => day.name === selectedDay);
 
-  const filteredAppArr = [];
-  for (const el of filteredDays[0].appointments) {
-    filteredAppArr.push(state.appointments[el]);
-  }
-  return filteredAppArr;
-}
+  return (matchingAppts.length === 0 ? [] : matchingAppts[0].appointments.map(id =>
+    state.appointments[id]));
 
-export function getInterviewersForDay(state, theDay) {
+};
+
+export function getInterviewersForDay(state, selectedDay) {
+
+  // if received no days object, return an empty array
   if (state.days.length === 0) {
     return [];
   }
 
-  const filteredDays = state.days.filter(day => day.name === theDay);
+  // return an empty array if there is no match, otherwise return an array of matching objects
+  const matchingInterviews = state.days.filter(day => day.name === selectedDay);
 
-  return (filteredDays.length === 0 ? [] : filteredDays[0].interviewers.map(interviewId => 
-    state.interviewers[interviewId]));
+  return (matchingInterviews.length === 0 ? [] : matchingInterviews[0].interviewers.map(id =>
+    state.interviewers[id]));
 
-}
-
+};
 
 export function getInterview(state, interview) {
   const interViewData = {};
@@ -35,7 +35,9 @@ export function getInterview(state, interview) {
     return null;
   }
 
+  // assign student k:v to interViewData
   interViewData.student = interview.student;
+
   for (const interviewer in state.interviewers) {
     if (interview.interviewer === state.interviewers[interviewer].id) {
       interViewData.interviewer = state.interviewers[interviewer];
