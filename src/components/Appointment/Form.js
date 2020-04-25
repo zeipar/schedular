bdac7bd51d9e07622d31641acb0b276184acdc6e
Junsung Park <zeipar@gmail.com>
2020-04-24 23:43:15 -0700

    added validation to check if interviewer is not selected

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -22,6 +22,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (!interviewer) {
+      setError("Interviewer must be assigned");
+      return;
+    }
     setError("");
   
     props.onSave(name, interviewer);
