f75591743193a7d079574743d744883740d7fcc7
Junsung Park <zeipar@gmail.com>
2020-04-24 23:08:42 -0700

    reverted tests from xit -> it for final testing.

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,13 +1,12 @@
-import { CYCLIC_KEY } from "@storybook/addon-actions/dist/constants";
-
 describe("Appointments", () => {
+  // reset DB and wait until DOM is ready
   beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     cy.contains("Monday");
   });
 
-  xit("should book an interview", () => {
+  it("should book an interview", () => {
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
@@ -16,7 +15,7 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
-  xit("should edit an interview", () => {
+  it("should edit an interview", () => {
     cy.get("[alt=Edit]").first().click({ force: true });
     cy.get("[alt='Tori Malcolm").click();
     cy.get("[data-testid=student-name-input]").clear().type("Jason Park");
